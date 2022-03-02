2132ff8aca6b44120a310a463b8128606939edfe
Prs2630 <poojakinholkar1998@gmail.com>
2022-03-02 19:22:30 +0530

    added thali app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Footer from './component/Footer'
+import HomeContainer from './component/HomeContainer'
+import HomeCrousel from './component/HomeCrousel'
+import Navbar from './component/Navbar'
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <HomeCrousel />
+     <HomeContainer/>
+     <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/component/HomeCrousel.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const HomeCrousel = () => {
+  return (
+    <header>
+    <div className='header-content'>
+      <div className='header-content_logo'>
+        <div>
+          {/* <img src='https://365psd.com/images/home/469/restaurant-logo-with-chef-35068.jpg' alt='' /> */}
+          <span>
+            <b>Taste</b>
+          </span>
+        </div>
+        <p>
+          <b>Restaurant & BBQ</b>
+        </p>
+      </div>
+
+      <div className='content-main'>
+        <h1>Delicious Thali for your cravings</h1>
+        <p>We made fresh and healthy meals with different recipes</p>
+        
+      </div>
+    </div>
+    <img className='header-img' src="https://imgk.timesnownews.com/story/veg-thali.gif?tr=w-1200,h-900" alt='' />
+    
+  </header>
+  )
+}
+
+export default HomeCrousel
